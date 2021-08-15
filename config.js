08151f74676afafddf846e9ff085334c5b6d2ec5
projectdcy <58021244+projectdcy@users.noreply.github.com>
2021-08-15 18:24:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let hanConfigData = {
     //这里填写你的QQ收藏分享地址
     "qqKey": "https://sharechain.qq.com/8caeae5408a27214efd4a7253b95fc5c",
     //这里填写自己的底部版权
-    "copyRight": "Copyright © 2018-2020 api.vvhan.com 版权所有",
+    "copyRight": "Copyright © 2018-2020 kanata 版权所有",
     // 这里填写ICP备案号
     "icpText": "苏ICP备172号"
 }
