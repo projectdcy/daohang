7dc98b3366707d1087a7b3a0bede40a15f56524a
projectdcy <58021244+projectdcy@users.noreply.github.com>
2021-08-15 18:22:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let hanConfigData = {
     //这里是QQ收藏API解析接口，默认即可
     "apiUrl": "https://api.vvhan.com/api/qqsc",
     //这里填写你的QQ收藏分享地址
-    "qqKey": "https://sharechain.qq.com/c5265467c3abd92b6b5886da33a726de",
+    "qqKey": "https://sharechain.qq.com/8caeae5408a27214efd4a7253b95fc5c",
     //这里填写自己的底部版权
     "copyRight": "Copyright © 2018-2020 api.vvhan.com 版权所有",
     // 这里填写ICP备案号
-    "icpText": "苏ICP备17017222号"
+    "icpText": "苏ICP备172号"
 }
